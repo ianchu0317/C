d458f59ac0c360e0bd01bab27b73ff0563a8c44f
Ian Chen <chenian317@gmail.com>
2021-12-31 14:31:39 -0300

    Exercise 37

---
+++ CodeForWin/Loop/Exercise 37/README.md
@@ -0,0 +1,13 @@
+# C program to find ones complement of a binary number
+
+Write a C program to input binary number from user and find ones complement of binary number using loop. How to find 1s complement of a binary number in C programming. Logic to find ones complement of binary number in C programming.
+
+```
+Input
+
+Input binary number: 01000011
+
+Output
+
+Ones complement: 10111100
+```
---
+++ CodeForWin/Loop/Exercise 37/Solution.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define BINARY_SIZE 8
+
+void _findComplementBinaryNumber(char *, char *);
+
+int main()
+{
+    /* User input binary number */
+    char binaryNumber[BINARY_SIZE];
+    
+    printf("Input binary number: ");
+    fgets(binaryNumber, sizeof(binaryNumber), stdin);
+    //binaryNumber[strlen(binaryNumber) - 1] = '\0';
+
+    /* New complement number */
+    char complementBinaryNumber[BINARY_SIZE];
+
+    _findComplementBinaryNumber(complementBinaryNumber, binaryNumber);
+
+    /* Print out result */
+    printf("\nComplement binary number: %s\n", complementBinaryNumber);
+
+    return 0;
+}
+
+/* Function to find complement binary number */
+void _findComplementBinaryNumber(char *complementBinaryNumber, char *binaryNumber)
+{
+    for (int i = 1; i <= BINARY_SIZE; i++)
+    {
+        if (binaryNumber[i - 1] == '1') {
+            complementBinaryNumber[i - 1] = '0';
+        } else if (binaryNumber[i - 1] == '0'){
+            complementBinaryNumber[i - 1] = '1';
+        }
+    }
+}
