ebe8ac7ff4ad2a07a26f8fa09089b8bc7797bd12
Ian Chen <chenian317@gmail.com>
2021-12-31 14:48:02 -0300

    Updated gitignore

---
+++ makeFile.py
@@ -0,0 +1,16 @@
+from os import listdir, system
+from time import sleep
+
+# Create file
+def createFiles():
+    pass
+
+# Create folders
+def createFolder(number):
+    pass
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
