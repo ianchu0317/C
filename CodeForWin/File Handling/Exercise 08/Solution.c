f4d9779ef25b3db74740662ff2ec1e1fe2a36812
Ian Chen <chenian317@gmail.com>
2022-01-12 10:53:35 -0300

    Exercise 08

--- CodeForWin/File Handling/Exercise 08/Solution.c
+++ CodeForWin/File Handling/Exercise 08/Solution.c
@@ -1,42 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "../essentials.h"
 
-void fileCounter(FILE *, int *, int *, int *);
+#ifndef MAX_SIZE
+#define MAX_SIZE 100
+#endif
 
 int main(int argc, char const *argv[]) {
+  /* User input file name */
   char fileName[MAX_SIZE];
-  inputFilename(fileName);
+  printf("Input file name to read: ");
+  fgets(fileName, sizeof(fileName), stdin);
+  fileName[strlen(fileName) - 1] = '\0';
 
+  /* try opening the file */
   FILE *file = fopen(fileName, "r");
-  checkFile(file);
+  if (file == NULL){
+    printf("[!] Failed opening file !\n");
+    exit(1);
+  }
 
+  /* Counter variables declaration */
   int charCounter, wordCounter, lineCounter;
   charCounter = wordCounter = lineCounter = 0;
-  fileCounter(file, &charCounter, &wordCounter, &lineCounter);
-
-  printf("\nChars: %d\n, Words: %d\n", charCounter);
-  printf("Words: %d\n", wordCounter);
-  printf("Lines: %d\n", lineCounter);
-  return 0;
-}
-
-void fileCounter(FILE *file, int charCounter, int * wordCounter, int *lineCounter){
-  // int charCounter, wordCounter, lineCounter;
-  // charCounter = wordCounter = lineCounter = 0;
   char c;
 
+  /* Read file char by char */
   c = fgetc(file);
 
   while (c != EOF){
     if (c == ' '){
-      *wordCounter++;
+      wordCounter++;
     } else if (c == '\n'){
-      *lineCounter++;
-    } else {
-      *charCounter++;
-    }
+      lineCounter++;
+      wordCounter++;
+    } /*¿else {
+      charCounter++;
+    }*/
+    charCounter++;
     c = fgetc(file);
   }
+
+  /*  Print out result */
+  printf("\nTotal characters: %d\n", charCounter);
+  printf("Total words: %d\n", wordCounter);
+  printf("Total lines: %d\n", lineCounter);
+  return 0;
 }
