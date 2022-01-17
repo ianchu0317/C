4d7074cc77992cefbd6f31dd8785014e63d549fc
Ian Chen <chenian317@gmail.com>
2022-01-17 18:51:09 -0300

    Exercise 01

---
+++ CodeForWin/Functions/Exercise 01/README.md
@@ -0,0 +1,10 @@
+# C program to find cube of a number using function
+```
+Input
+
+Input any number: 5
+
+Output
+
+Cube of 5 = 125
+```
--- CodeForWin/Functions/Exercise 01/Solution.c
+++ CodeForWin/Functions/Exercise 01/Solution.c
@@ -1,10 +1,22 @@
 #include <stdio.h>
 
+int cube(int n){
+  int result = 1;
+  for (int i = 1; i <= 3; i++){
+    result *= n;
+  }
+  return result;
+}
+
+
 int main () {
+  /* User input number */
   int num;
-  printf("Input number: ");
+  printf("Input any number: ");
   scanf("%d", &num);
-  
-  printf()
+
+  /* Print out result */
+  printf("Cube of %d = %d\n", num, cube(num));
+
   return 0;
 }
