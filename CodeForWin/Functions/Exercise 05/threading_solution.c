a81b7c90f8bbaabf1d6fa71197347a630c53067e
Ian Chen <ianchu0317@gmail.com>
2022-01-21 00:41:27 -0300

    Exercise 05 with threading

--- CodeForWin/Functions/Exercise 05/threading_solution.c
+++ CodeForWin/Functions/Exercise 05/threading_solution.c
@@ -3,135 +3,196 @@
 #include <math.h>
 #include <pthread.h>
 
+// 3 Threads
 #define THREADS 3
 
-// Struct to limits, lower limit and upper limit
-struct parameter{
-  long long lowerLimit;
-  long long upperLimit;
+// Parameters struct 
+struct parameters{
+  long long min;
+  long long max;
 };
 
-void* checkArmstrong(void *argv);
-void* checkPerfect(void *argv);
-void* findStrong(void *argv);
+// Struct to store answers and the result
+struct answers{
+  long long *result;
+  long long counter;
+};
 
+// Functions
+void *find_armstrong_number(void *);
+void *find_perfect_number(void *);
+void *find_perfect_number(void *);
+void print_result(struct answers *);
+
+
+// Main function
 int main(int argc, char *argv[]){
-  // User input data, exit if not provided argument
-  if (argc < 3){
-    perror("\nUsage: <program> <lower limit> <upper limit>\n");
-    exit(1);
-  }
+  // User input data 
+  struct parameters args;
+  printf("Input lower limit: ");
+  scanf("%lld", &args.min);
+  printf("Input upper limit: ");
+  scanf("%lld", &args.max);
 
-  // Convert to long long variable type
-  struct parameter limits[THREADS];
-  for (int i = 1; i <= THREADS; i++){
-    limits[i-1].lowerLimit = atoll(argv[1]);
-    limits[i-1].upperLimit = atoll(argv[2]);
-  }
+  // Thread id declaration
+  pthread_t tid[THREADS];
+  int t_counter = 0;
+  
+  // Start searching armstrong numbers
+  pthread_create(&tid[t_counter], NULL, find_armstrong_number, (void *) &args);
+  t_counter += 1;
+  // Start searching perfect numbers
+  pthread_create(&tid[t_counter], NULL, find_perfect_number, (void *) &args);
+  t_counter += 1;
+  // Start searching strong numbers
+  pthread_create(&tid[t_counter], NULL, find_strong_number, (void *) &args);
 
-  // Define thread
-  pthread_t thread_id[THREADS];
-  int thread_count = 0;
+  t_counter = 0; 
 
-  long long *strong_numbers;
-  long long 
-  printf("Strong Numbers between %lld to %lld are: ", limits[thread_count].lowerLimit, limits[thread_count].upperLimit);
+  // Struct to storage data 
+  struct answers *armstrong_numbers;
+  struct answers *perfect_numbers;
+  struct answers *strong_numbers;
+  
+  pthread_join(tid[t_counter], (void **) &armstrong_numbers);
+  t_counter += 1;
+  pthread_join(tid[t_counter], (void **) &perfect_numbers);
+  t_counter += 1;
+  pthread_join(tid[t_counter], (void **) &strong_numbers);
 
-  printf("Perfect Numbers between %lld to %lld are: ", limits[thread_count].lowerLimit, limits[thread_count].upperLimit);
-  printf("Perfect numbers between %lld to %lld are: ", limits[thread_count].lowerLimit, limits[thread_count].upperLimit);
+
+  // Print out result 
+  printf("\nArmstrong numbers between %lld to %lld are: ", args.min, args.max);
+  print_result(armstrong_numbers);
+
+  printf("\nPerfect numbers between %lld to %lld are: ", args.min, args.max);
+  print_result(perfect_numbers);
+
+  printf("\nArmstrong numbers between %lld to %lld are: ", args.min, args.max);
+  print_result(strong_numbers);
+  
+
+  // Free memory
+  free(armstrong_numbers->result);
+  free(armstrong_numbers);
+  free(perfect_numbers->result);
+  free(perfect_numbers);
+  free(strong_numbers->result);
+  free(strong_numbers);
 
   return 0;
 }
 
+/********************************************************************/
+// Function to print out result 
+void print_result(struct answers *answer){
+  for (int i = 1; i < answer->counter; i++){
+    printf("%lld, ", answer->result[i-1]);
+  }
+  printf("\n");
+}
 
-// Function to find strong Numbers
-void *findStrong(void *argv){
-  struct parameter *limit = (struct parameter *) argv;
+// Function to find armstrong numbers
+void *find_armstrong_number(void *ptr){
+  struct parameters *limits = (struct parameters *) ptr;
 
-  int current_number;
-  long long copy_num;
-  long long factorial;
-  long long sum;
+  struct answers *answer = (struct answers *) malloc(sizeof(struct answers));
+  answer->counter = 1;
+  answer->result = (long long *) malloc(sizeof(long long) * answer->counter);
 
-  // For each number
-  for (long long i = limit->lowerLimit; i <= limit->upperLimit; i++){
-    // Reset variables
-    copy_num = i;
+  int sum; // Sum power of digits
+  int currentNumber, currentDigit; // Copy of the current number and curretn digit
+  int digitsCounter;
+
+  // For each number in range
+  for (long long i = limits->min; i <= limits->max; i++){
+    currentNumber = i;
+    digitsCounter = 0;
     sum = 0;
 
-    // For each digit
-    while (copy_num != 0) {
+    while (currentNumber != 0){
+      currentNumber /= 10;
+      digitsCounter += 1;
+    }
 
-      current_number = (int) copy_num % 10;
+    currentNumber = i;
+
+    while (currentNumber != 0){
+      currentDigit = currentNumber % 10;
+      sum += pow(currentDigit, digitsCounter);
+      currentNumber /= 10;
+    }
+
+    if (sum == i){
+      answer->result[answer->counter - 1] = i;
+      answer->counter += 1;
+      answer->result = realloc(answer->result, sizeof(long long) * answer->counter);
+    }
+  }
+
+  pthread_exit(answer);
+}
+
+// Function to find perfect number
+void *find_perfect_number(void * args){
+  struct parameters *limits = (struct parameters *) ptr;
+
+  struct answers *answer = (struct answers *) malloc(sizeof(struct answers));
+  answer->counter = 1;
+  answer->result = (long long *) malloc(sizeof(long long) * answer->counter);
+  
+  long long sum;
+  // For each number
+  for (long long i = limits->min; i <= limits->max; i++){
+    sum = 0;
+    
+    for (int x = 1; x <= i / 2; x ++){
+      if (i % x == 0)
+        sum += x;
+    }
+
+    // Store data in array    
+    if (sum == i){
+      answer->result[answer->counter - 1] = i;
+      answer->counter += 1;
+      answer->result = realloc(answer->result, sizeof(long long) * answer->counter);
+    }
+
+  }
+  
+  pthread_exit(answer);
+}
+
+void *find_strong_number(void *args){
+  struct parameters *limits = (struct parameters *) ptr;
+
+  struct answers *answer = (struct answers *) malloc(sizeof(struct answers));
+  answer->counter = 1;
+  answer->result = (long long *) malloc(sizeof(long long) * answer->counter);
+  long long sum = 0;
+  long long n, factorial;
+ 
+  for (long long i = limits->min; i <= limits->max; i++){
+     /* For all digits of number */
+    n = i;
+    while (n != 0){
+      /* Find sum of factorials*/
       factorial = 1;
-
-      //Find factorial
-      while (current_number > 0){
-        factorial *= current_number;
-        current_number -= 1;
+      for (int x = n % 10; x >= 1; x--){
+        factorial *= x;
       }
       sum += factorial;
-
-      copy_num /= 10;
+      n /= 10;
     }
 
-    //Save if found
+    /* Print number if its strong number */
     if (sum == i){
-      printf("%d, ", i);
+      answer->result[answer->counter - 1] = i;
+      answer->counter += 1;
+      answer->result = realloc(answer->result, sizeof(long long) * answer->counter);
     }
-  }
-  printf("\n\n");
-}
-
-void* checkArmstrong(void *argv){
-  struct parameter *limit = (struct parameter *) argv;
-  /* Find number digits len */
-  int digitsCounter = 0;
-  int power;
-  long long sum;
-  long long tempNum;
-  for (long long i = limit->lowerLimit; i <= limit->upperLimit; i++){
-    tempNum = i;
-    while (tempNum != 0){
-      tempNum /= 10;
-      digitsCounter++;
-    }
-    /* Sum the power of digits to digits length */
-    power = digitsCounter;
     sum = 0;
-    tempNum = i;
-
-    while (tempNum != 0){
-      sum += pow((tempNum % 10), power);
-      tempNum /= 10;
-      digitsCounter++;
-    }
-
-    if (sum == i){
-        printf("%d, ", i);
-    }
   }
-
-  printf("\n\n");
-}
-
-/* Check perfect number */
-void* checkPerfect(void *argv){
-
-  struct parameter *limit = (struct parameter *) argv;
-  long long sum;
-
-  for (long long i = limit->lowerLimit; i <= limit->upperLimit; i++){
-    sum = 0;
-    for (long long x = 1; x <= n / 2; x ++){
-      if (i % x == 0){
-        sum += x;
-      }
-    }
-
-    if (sum == i){
-      printf("%d, ", i);
-    }
-  }
-  printf("\n\n");
+  
+  pthread_exit(answer);
 }
