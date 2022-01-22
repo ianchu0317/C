df618baf72f583e89c7cb65d55a5192ecf7d5e91
Ian Chen <chenian317@gmail.com>
2022-01-21 23:34:51 -0300

    Exercise 11

---
+++ CodeForWin/Arrays/Exercise 11/README.md
@@ -0,0 +1,16 @@
+# C program to count frequency of each element in an arrays
+
+```
+Input
+
+Input array elements: 5, 10, 2, 5, 50, 5, 10, 1, 2, 2
+
+Output
+
+Frequency of 5 = 3
+Frequency of 10 = 2
+Frequency of 2 = 3
+Frequency of 50 = 1
+Frequency of 1 = 1
+```
+
---
+++ CodeForWin/Arrays/Exercise 11/Solution.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+int main(){
+    // User input data
+    int num;
+    printf("Input number of elements to store in array: ");
+    scanf("%d", &num);
+
+    int array[num];
+
+    printf("Input %d numbers of elements in array:\n", num);
+    for (int i = 1; i <= num; i++) scanf("%d", &array[i-1]);
+
+    // New array and enter position to delete
+    int frequency[num][2];
+    int found_num = 0;
+    int freq_count = 0;
+    /*
+    it should look something like this
+    element 1: 1
+    element 2: 2
+    etc...
+    */
+
+    // For each number in the first array
+    for (int i = 1; i <= num; i++){
+      // Count frecuency
+      for (int x = 1; x <= num; x++){
+        if (array[i-1] == array[x-1]){
+          freq_count += 1;
+        }
+
+      }
+      for (int x = 1; x <= i; x++){
+        if (frequency[x][0] == array[i-1]) break;
+        else if (frequency[x-1][0] != array[i-1] && x == i){
+          frequency[found_num][0] = array[i-1];
+          frequency[found_num][1] = freq_count;
+          found_num += 1;
+        }
+      }
+      freq_count = 0; // reset counter
+    }
+
+    // Output
+    printf("\n\n");
+    while (found_num != 0) {
+      printf("Frequency of %d: %d\n", frequency[found_num - 1][0], frequency[found_num - 1][1]);
+      found_num -= 1;
+    }
+
+    return 0;
+}
