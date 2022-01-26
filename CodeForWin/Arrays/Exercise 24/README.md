74b2677bd929c459a9a9ee8512b473c5ea1ca2e1
Ian Chen <chenian317@gmail.com>
2022-01-26 08:23:42 -0300

    Exercise 24

---
+++ CodeForWin/Arrays/Exercise 24/README.md
@@ -0,0 +1,23 @@
+# C program to subtract two matrices
+
+```
+Input
+
+Input elements in 3x3 matrix1:
+1 2 3
+4 5 6
+7 8 9
+
+Input elements in 3x3 matrix2:
+9 8 7
+6 5 4
+3 2 1
+
+
+Output
+
+Difference of both matrices =
+-8 -6 -4
+-2  0  2
+4  6  8
+```
---
+++ CodeForWin/Arrays/Exercise 24/Solution.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+// Function to store matrix from user input
+void input_matrix(int matrix[3][3]){
+  for (int i = 1; i <= 3; i++){   // Each row
+    for (int x = 1; x <= 3; x++){ // Each column
+      scanf("%d", &matrix[i-1][x-1]);
+    }
+  }
+}
+
+// Function to display substract of both matrix
+void display_sum_matrix(int m1[3][3], int m2[3][3]){
+  for (int i = 1; i <= 3; i++){ // Row
+    for (int x = 1; x <= 3; x++){ // Column
+      printf("%d ", (m1[i-1][x-1] - m2[i-1][x-1])); // Print out
+    }
+    printf("\n");
+  }
+}
+
+
+int main()
+{
+  // User input data
+  int matrix1[3][3];
+  printf("Input elements in 3x3 matrix1:\n");
+  input_matrix(matrix1);
+
+  int matrix2[3][3];
+  printf("Input elements in 3x3 matrix2:\n");
+  input_matrix(matrix2);
+
+  // Substract matrix without storing to var
+  printf("Sum of both matrix:\n");
+  display_sum_matrix(matrix1, matrix2);
+
+  return 0;
+}
